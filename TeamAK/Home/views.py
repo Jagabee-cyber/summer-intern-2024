ec2150d185f8ad9c90229d80b0b2c48498e4cc92
Zach <zacharywang405@gmail.com>
2024-08-05 14:17:19 -0700

    Add teamAK data

---
+++ TeamAK/Home/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def login(request):
+    return render(request, "login.html")
+
+def createAccount(request):
+    return render(request, "createAcc.html")
